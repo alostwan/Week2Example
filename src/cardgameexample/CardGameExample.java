9473d354db885785fd6b80895ce6c21ccc62c200
alostwan <alostwan@sheridancollege.ca>
2021-06-01 11:26:42 -0400

    First Commit

---
+++ src/cardgameexample/CardGameExample.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgameexample;
+
+import java.util.Scanner;
+
+/**
+ * interaction -I/O
+ * @author sivagamasrinivasan 
+ * date: 25.05.2021
+ */
+public class CardGameExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       // take and store only 4 cards - next week deck 52 
+        
+        Card[] deck = new Card[52]; // array of Card object- suit and value
+        Scanner input= new Scanner(System.in);
+        System.out.println(" enter your card details :");
+        for(int i=0;i<deck.length;i++)
+        {
+//            Card c1 = new Card("card1" , 45);
+//            deck[0]=c1; // object of the card is saved in array
+
+            deck[i]   =   new Card("NBA" , i+1);
+            
+        }
+    for(int i=0;i<deck.length;i++)
+        {
+         System.out.println(deck[i].getSuit() + "   "+ deck[i].getValue());
+                
+ 
+        }
+    
+}
+}
